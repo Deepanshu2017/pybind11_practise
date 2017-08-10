f06b09b38cf100f9dad9cba99daf094abb45eb93
Deepanshu <deepanshu2017@hotmail.com>
2017-08-11 03:59:55 +0530

    Added an example to show the change in values
    Signed-off-by: Deepanshu <deepanshu2017@hotmail.com>

--- 34/foo.cpp
+++ 34/foo.cpp
@@ -1,18 +1,49 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
 #include <iostream>
 #include <vector>
 
 namespace py = pybind11;
 
 struct Foo {
-  void print(int a, bool inverse = false) {
-    std::cout << "a: " << a << " inverse: " << inverse << std::endl;
+private:
+  int var;
+public:
+  void setvar(int x) { var = x; }
+  void getvar() { std::cout << var << std::endl; }
+  void print(std::vector<Foo>& s) {
+    for (auto &i : s)
+      i.setvar(10);
   }
 };
 
+PYBIND11_MAKE_OPAQUE(std::vector<Foo>);
+
 PYBIND11_MODULE(foo, m) {
+  py::bind_vector<std::vector<Foo>>(m, "VectorFoo");
   py::class_< Foo >(m, "Foo")
     .def(py::init<>())
-    .def("print", &Foo::print, py::arg("a"), py::arg("inverse") = false)
+    .def("print", &Foo::print)
+    .def("set", &Foo::setvar)
+    .def("get", &Foo::getvar)
   ;
 }
+
+/*
+In [1]: import foo
+
+In [2]: l = [foo.Foo(), foo.Foo(), foo.Foo()]
+
+In [3]: l = foo.VectorFoo(l)
+
+In [4]: l
+Out[4]: <foo.VectorFoo at 0x7f1375383990>
+
+In [5]: a = foo.Foo()
+
+In [6]: a.print(l)
+
+In [7]: l[0].get()
+10
+*/
